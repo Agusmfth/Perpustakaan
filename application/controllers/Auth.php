ad0ef867e8fcb4d7d7bcbd905a196cd4bda2900d
Agusmfth <agusmiftach2013@gmail.com>
2023-02-18 00:02:28 +0700

    mengubah fitur login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller 
 {
     public function index(){
+        $this->load->view('templates/header_auth');
         $this->load->view('auth/login');
+        $this->load->view('templates/footer_auth');
     }
 }
